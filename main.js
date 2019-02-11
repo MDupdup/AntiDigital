e2584f77baf07ab8374f618b3215ace0b9d01fc5
Malo Dupont <malo.dupont@ynov.com>
2019-02-11 23:27:02 +0100

    changes

--- main.js
+++ main.js
@@ -6,23 +6,31 @@ if(["fr", "fr-FR"].indexOf(document.documentElement.lang) > -1) {
     var textNode;
     var words = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
-    const reg = new RegExp('digitale|digital', 'gi');
+    const regSng = new RegExp('digitale|digital', 'gi');
+    const regPlr = new RegExp('digitales|digitaux', 'gi');
+
+    const regSngUp = new RegExp('Digitale|Digital', 'gi');
+    const regPlrUp = new RegExp('Digitales|Digitaux', 'gi');
+
+
 
     if(blacklistedSites.indexOf(window.location.host) === -1) {
+        console.log("Hein"+ window.location.host);
         console.log("Y'a-t-il du digital par ici ?");
-        let previousNode = "";
         while(textNode = words.nextNode()) {
-            if(["marketing", "business", ""].indexOf(textNode.nextSibling.toLowerCase()) > -1) {
-                console.log("ah, un vrai digital");
-                previousNode = textNode.nodeValue;
-                continue;
-            } 
+            if(words.nextSibling() !== null) {
+                if(["marketing", "business", ""].indexOf(words.nextSibling().nodeValue.toLowerCase()) > -1) {
+                    console.log("ah, un vrai digital");
+                    continue;
+                }
+            }
+            console.log("ça dégage");
+            textNode.nodeValue = textNode.nodeValue.replace(regSng, 'numérique');
+            textNode.nodeValue = textNode.nodeValue.replace(regPlr, 'numériques');
 
-            previousNode = textNode.nodeValue;
-
-            textNode.nodeValue = textNode.nodeValue.replace(reg, 'numérique');
+            textNode.nodeValue = textNode.nodeValue.replace(regSngUp, 'Numérique');
+            textNode.nodeValue = textNode.nodeValue.replace(regPlrUp, 'Numériques');
         }
-    
-        document.title = document.title.replace(reg, 'numérique');
+        document.title = document.title.replace(regSng, 'numérique');
     }
 }
