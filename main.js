09b59df7b693b2a8d3f66014d42cff5da1a5cfa2
mdupdup <malo.dupont@ynov.com>
2019-02-12 11:48:36 +0100

    working!

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 let blacklistedSites = [
-    "fr.wikipedia.org",
+    "fr.wikipedia.org"
 ];
 
 if(["fr", "fr-FR"].indexOf(document.documentElement.lang) > -1) {
     var textNode;
-    var words = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+    var paragraphs = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
-    const regSng = new RegExp('digitale|digital', 'gi');
-    const regPlr = new RegExp('digitales|digitaux', 'gi');
+    const regSng = new RegExp('digitale|digital', 'g');
+    const regPlr = new RegExp('digitales|digitaux', 'g');
 
-    const regSngUp = new RegExp('Digitale|Digital', 'gi');
-    const regPlrUp = new RegExp('Digitales|Digitaux', 'gi');
+    const regSngUp = new RegExp('Digitale|Digital', 'g');
+    const regPlrUp = new RegExp('Digitales|Digitaux', 'g');
 
+    let words;
+    let endChar;
+    let tmpPunct;
 
 
     if(blacklistedSites.indexOf(window.location.host) === -1) {
-        console.log("Hein"+ window.location.host);
         console.log("Y'a-t-il du digital par ici ?");
-        while(textNode = words.nextNode()) {
-            if(words.nextSibling() !== null) {
-                if(["marketing", "business", ""].indexOf(words.nextSibling().nodeValue.toLowerCase()) > -1) {
-                    console.log("ah, un vrai digital");
+        while(textNode = paragraphs.nextNode()) {
+            words = textNode.nodeValue.split(' ');
+            for(var i = 0; i < words.length; i++) {
+                if(words[i+1] !== undefined && ["marketing", "business"].indexOf(words[i+1].toLowerCase()) > -1) {
                     continue;
+                } else if(new RegExp(['digitale', 'digital', 'digitales', 'digitaux'].join("|")).test(words[i].toLowerCase())) {
+                    endChar = words[i].split('')[words[i].length-1];
+                    if([',', '.', '!', '?', '"', '\''].indexOf(endChar) > -1) {
+                        if(words[i].split('')[words[i].length-2] === ".") {
+                            tmpPunct = "...";
+                            words[i] = words[i].substring(0, words[i].length-3);
+                        } else {
+                            tmpPunct = endChar;
+                            words[i] = words[i].substring(0, words[i].length-1);
+                        }
+                    } else {
+                        tmpPunct = "";
+                    }
+
+                    words[i] = words[i].replace(regSng, 'numérique' + tmpPunct);
+                    words[i] = words[i].replace(regPlr, 'numériques' + tmpPunct);
+        
+                    words[i] = words[i].replace(regSngUp, 'Numérique' + tmpPunct);
+                    words[i] = words[i].replace(regPlrUp, 'Numériques' + tmpPunct);
                 }
             }
-            console.log("ça dégage");
-            textNode.nodeValue = textNode.nodeValue.replace(regSng, 'numérique');
-            textNode.nodeValue = textNode.nodeValue.replace(regPlr, 'numériques');
-
-            textNode.nodeValue = textNode.nodeValue.replace(regSngUp, 'Numérique');
-            textNode.nodeValue = textNode.nodeValue.replace(regPlrUp, 'Numériques');
+            textNode.nodeValue = words.join(' ');
         }
         document.title = document.title.replace(regSng, 'numérique');
     }
